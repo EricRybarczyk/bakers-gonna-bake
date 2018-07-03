f74ea91f0679dcbb68ec8bb9dacbac3418120f07
EricRybarczyk <EricRybarczyk@gmail.com>
2018-07-03 19:31:27 -0400

    Added missing field to Step object

--- app/src/main/java/com/example/ericr/bakersgonnabake/model/Step.java
+++ app/src/main/java/com/example/ericr/bakersgonnabake/model/Step.java
@@ -2,12 +2,14 @@ package com.example.ericr.bakersgonnabake.model;
 
 public class Step {
     private int id;
+    private String shortDescription;
     private String description;
     private String videoSource;
     private String thumbnameImage;
 
-    public Step(int id, String description, String videoSource, String thumbnameImage) {
+    public Step(int id, String shortDescription, String description, String videoSource, String thumbnameImage) {
         this.id = id;
+        this.shortDescription = shortDescription;
         this.description = description;
         this.videoSource = videoSource;
         this.thumbnameImage = thumbnameImage;
@@ -16,6 +18,8 @@ public class Step {
     public int getId() {
         return id;
     }
+
+    public String getShortDescription() { return shortDescription; }
 
     public String getDescription() {
         return description;
